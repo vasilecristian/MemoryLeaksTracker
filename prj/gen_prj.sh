124cd09013249dfc2652780c4b63299e0a60a682
Cristian Vasile <vasile.cristian@gmail.com>
2022-04-19 13:32:59 +0300

    Removed unneeded info.

--- prj/gen_prj.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-#
-chmod +x ../../premake/release/premake5
-../../premake/release/premake5 --arch=ios --to=xcode4/ios xcode4
-../../premake/release/premake5 --arch=macosx --to=xcode4/macosx xcode4
--- prj/gen_prj_linux.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-chmod +x ../../premake/release/premake5_linux
-../../premake/release/premake5_linux --arch=x32 --to=gmake gmake
